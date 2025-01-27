6f791dbf9525f74c5f81fac98b234bb13581e2d8
KaePee <obedpksomuah@gmail.com>
2025-01-27 01:52:29 +0000

    setup: earlyWithdraw

--- test/SoccersmWithdraw.ts
+++ test/SoccersmWithdraw.ts
@@ -29,7 +29,7 @@ import {
 } from "./lib";
 
 describe("ChallengePool - Withdraw And Fees", function () {
-    it("Should [Withdraw]", async function () {
+    it("Should [earlyWithdraw]", async function () {
           const {
             oneGrand,
             baller,
@@ -41,5 +41,59 @@ describe("ChallengePool - Withdraw And Fees", function () {
             paymaster,
           } = await loadFixture(deploySoccersm);
 
+          //Setup: Create and Stake a challenge
+          const btcChallenge = btcEvent(
+            await ballsToken.getAddress(),
+            1,
+            oneGrand,
+            ethers.ZeroAddress
+          );
+          const preparedBTCChallenge = prepareCreateChallenge(
+            btcChallenge.challenge
+          );
+    
+          await ballsToken
+            .connect(baller)
+            .approve(
+              await poolHandlerProxy.getAddress(),
+              (
+                await poolHandlerProxy.createFee(oneGrand)
+              )[1]
+            );
+          await (poolHandlerProxy.connect(baller) as any).createChallenge(
+            ...(preparedBTCChallenge as any)
+          );
+
+          const challengeId = await poolManagerProxy.challengeId() - 1n;
+
+          const maxPrice = oneGrand * 2n;
+          const prediction = ethers.AbiCoder.defaultAbiCoder().encode(["string"], ["yes"]);
+
+          const stakeParams = {
+            _challengeId: challengeId,
+            _prediction: prediction,
+            _quantity: 1,
+            _maxPrice: maxPrice,
+            _deadline: (await time.latest()) + 1000,
+            _paymaster: ethers.ZeroAddress,
+        };
+        
+        await ballsToken
+          .connect(baller)
+          .approve(
+            await poolHandlerProxy.getAddress(),
+            maxPrice
+          )
+          //should stake
+        await expect((poolHandlerProxy.connect(baller) as any).stake(
+         stakeParams._challengeId,
+         stakeParams._prediction,
+         stakeParams._quantity,
+         stakeParams._maxPrice,
+         stakeParams._deadline,
+         stakeParams._paymaster 
+        )).to.not.be.reverted;
+
+        
     });
 })
