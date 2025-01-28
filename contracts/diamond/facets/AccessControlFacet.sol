0967629b7ed6b2c0efe8c2899bdaa48700d8af02
baahkusi <nanakbaahkusi@gmail.com>
2025-01-28 03:39:35 +0000

    lib role

--- contracts/diamond/facets/AccessControlFacet.sol
+++ contracts/diamond/facets/AccessControlFacet.sol
@@ -3,5 +3,11 @@ pragma solidity ^0.8.28;
 
 import "@solidstate/contracts/access/access_control/AccessControl.sol";
 import "@solidstate/contracts/access/access_control/AccessControlStorage.sol";
-
-contract AccessControlFacet is AccessControl {}
+import "../libraries/LibRole.sol";
+contract AccessControlFacet is AccessControl {
+    bytes32 public constant ORACLE_ROLE = LibRole.ORACLE_ROLE;
+    bytes32 public constant SOCCERSM_COUNCIL = LibRole.SOCCERSM_COUNCIL;
+    bytes32 public constant CHALLENGE_POOL_MANAGER =
+        LibRole.CHALLENGE_POOL_MANAGER;
+    bytes32 public constant TOPIC_REGISTRAR = LibRole.TOPIC_REGISTRAR;
+}
--- contracts/diamond/interfaces/SoccersmRoles.sol
+++ contracts/diamond/interfaces/SoccersmRoles.sol
@@ -2,35 +2,30 @@
 pragma solidity ^0.8.28;
 
 import "@solidstate/contracts/access/access_control/AccessControlInternal.sol";
+import "../libraries/LibRole.sol";
 abstract contract SoccersmRoles is AccessControlInternal {
-    bytes32 internal constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
-    bytes32 internal constant SOCCERSM_COUNCIL = keccak256("SOCCERSM_COUNCIL");
-    bytes32 internal constant CHALLENGE_POOL_MANAGER =
-        keccak256("CHALLENGE_POOL_MANAGER");
-    bytes32 internal constant TOPIC_REGISTRAR = keccak256("TOPIC_REGISTRAR");
-
     modifier onlyAdmin() {
         _checkRole(AccessControlStorage.DEFAULT_ADMIN_ROLE);
         _;
     }
 
     modifier onlyOracle() {
-        _checkRole(ORACLE_ROLE);
+        _checkRole(LibRole.ORACLE_ROLE);
         _;
     }
 
     modifier onlySoccersmCouncil() {
-        _checkRole(SOCCERSM_COUNCIL);
+        _checkRole(LibRole.SOCCERSM_COUNCIL);
         _;
     }
 
     modifier onlyPoolManager() {
-        _checkRole(CHALLENGE_POOL_MANAGER);
+        _checkRole(LibRole.CHALLENGE_POOL_MANAGER);
         _;
     }
 
     modifier onlyTopicRegistrar() {
-        _checkRole(TOPIC_REGISTRAR);
+        _checkRole(LibRole.TOPIC_REGISTRAR);
         _;
     }
 }
