6780877a74c009fad642434bc560961f01bffbf8
KaePee <obedpksomuah@gmail.com>
2025-01-28 15:50:10 +0000

    inherit

--- contracts/diamond/upgradeInitializers/AccessControlInit.sol
+++ contracts/diamond/upgradeInitializers/AccessControlInit.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.28;
 import "@solidstate/contracts/access/access_control/AccessControlInternal.sol";
 import "@solidstate/contracts/access/access_control/AccessControlStorage.sol";
 import "../interfaces/SoccersmRoles.sol";
+import "../../diamond/facets/AccessControlFacet.sol";
 
-contract AccessControlInit is AccessControlInternal, SoccersmRoles {
+contract AccessControlInit is AccessControlInternal, AccessControlFacet, SoccersmRoles {
     function init() external {
         _grantRole(AccessControlStorage.DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ORACLE_ROLE, msg.sender);
