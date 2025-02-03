f506ddda222fcef92780fe2253c900319cdb885a
KaePee <obedpksomuah@gmail.com>
2025-02-03 23:54:13 +0000

    settle dispute

--- test/SoccersmDispute.ts
+++ test/SoccersmDispute.ts
@@ -30,6 +30,7 @@ import {
   getChallengeState,
   getPlayerDisputes,
   getPlayerOptionSupply,
+  getPoolDisputes,
 } from "./test_helpers";
 import { bigint } from "hardhat/internal/core/params/argumentTypes";
 
@@ -250,5 +251,218 @@ describe("ChallengePool - Dispute", function () {
         await time.increase(60 * 60);
         await expect((poolDisputeProxy.connect(striker) as any).dispute(0, loosingPrediction1)).to.be.reverted;
 
+  });
+
+   it("Should [Settle]", async function () {
+    const {
+      oneGrand,
+      oneMil,
+      baller,
+      striker,
+      ballsToken,
+      poolHandlerProxy,
+      registryProxy,
+      poolViewProxy,
+      poolDisputeProxy,
+      poolManagerProxy,
+      keeper,
+      paymaster,
+    } = await loadFixture(deploySoccersm);
+
+    // Settle: Setup
+      /* create challenge
+      stake challenge
+      mature challenge
+      dispute challenge
+      settle
+      */
+
+    // create challenge
+    const ch = btcEvent(
+          await ballsToken.getAddress(),
+          1,
+          oneGrand,
+          ethers.ZeroAddress
+        );
+    
+        const winningPrediction = yesNo.no;
+        const loosingPrediction1 = yesNo.yes;
+        const loosingPrediction2 = yesNo.yes;
+        const assetPrice = 110000 * ASSET_PRICES_MULTIPLIER;
+    
+        const preparedMultiStementChallenge = prepareCreateChallenge(ch.challenge);
+    
+        await ballsToken
+          .connect(baller)
+          .approve(
+            await poolHandlerProxy.getAddress(),
+            (
+              await poolViewProxy.createFee(oneGrand)
+            )[1]
+          );
+        await (poolHandlerProxy.connect(baller) as any).createChallenge(
+          ...(preparedMultiStementChallenge as any)
+        );
+        expect(await poolManagerProxy.challengeId()).to.equal(1);
+
+        await ballsToken
+          .connect(baller)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+    
+        //striker can stake
+        await ballsToken
+          .connect(striker)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+        await (poolHandlerProxy.connect(striker) as any).stake(
+          0,
+          loosingPrediction1,
+          2,
+          ethers.ZeroAddress
+        );
+    
+        await ballsToken.approve(await poolHandlerProxy.getAddress(), oneMil);
+        await poolHandlerProxy.stake(0, loosingPrediction2, 3, ethers.ZeroAddress);
+
+        //baller can stake again
+        await (poolHandlerProxy.connect(baller) as any).stake(
+          0,
+          winningPrediction,
+          1,
+          ethers.ZeroAddress
+        );
+    
+        await ballsToken.transfer(keeper, oneMil);
+        await ballsToken
+          .connect(keeper)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+    
+        await time.increaseTo(ch.maturity);
+        const provideDataParams = prepareAssetPriceProvision(
+          ch.assetSymbol,
+          ch.maturity,
+          assetPrice
+        );
+        await registryProxy.provideData(...provideDataParams);
+
+        //revert for not evaluated
+        await expect((poolDisputeProxy.connect(striker) as any).dispute(0, loosingPrediction1)).to.be.reverted;
+
+        //evaluate
+        await poolHandlerProxy.evaluate(0);
+
+        //revert keeper disputes(no stakes)
+        await expect((poolDisputeProxy.connect(keeper) as any).dispute(0, loosingPrediction1)).to.be.reverted;
+
+        //revert for invalid outcome
+        const invalidOutcome = coder.encode(["string"], ["invalid"]);
+         await expect((poolDisputeProxy.connect(keeper) as any).dispute(0, invalidOutcome)).to.be.reverted;
+
+        //revert for past time for dispute(1hour)
+        await time.increase(60 * 60);
+        await expect((poolDisputeProxy.connect(striker) as any).dispute(0, loosingPrediction1)).to.be.reverted;
+
+  });
+   it("Should [Settle]", async function () {
+    const {
+      oneGrand,
+      oneMil,
+      baller,
+      striker,
+      ballsToken,
+      poolHandlerProxy,
+      registryProxy,
+      poolViewProxy,
+      poolDisputeProxy,
+      poolManagerProxy,
+      keeper,
+      paymaster,
+    } = await loadFixture(deploySoccersm);
+
+    // Dispute: Setup
+      /* create challenge
+      stake challenge
+      mature challenge
+      dispute
+      settle 
+      */
+
+    // create challenge
+    const ch = btcEvent(
+          await ballsToken.getAddress(),
+          1,
+          oneGrand,
+          ethers.ZeroAddress
+        );
+    
+        const winningPrediction = yesNo.no;
+        const loosingPrediction1 = yesNo.yes;
+        const loosingPrediction2 = yesNo.yes;
+        const assetPrice = 110000 * ASSET_PRICES_MULTIPLIER;
+    
+        const preparedMultiStementChallenge = prepareCreateChallenge(ch.challenge);
+    
+        await ballsToken
+          .connect(baller)
+          .approve(
+            await poolHandlerProxy.getAddress(),
+            (
+              await poolViewProxy.createFee(oneGrand)
+            )[1]
+          );
+        await (poolHandlerProxy.connect(baller) as any).createChallenge(
+          ...(preparedMultiStementChallenge as any)
+        );
+        expect(await poolManagerProxy.challengeId()).to.equal(1);
+
+        await ballsToken
+          .connect(baller)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+    
+        //striker can stake
+        await ballsToken
+          .connect(striker)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+        await (poolHandlerProxy.connect(striker) as any).stake(
+          0,
+          loosingPrediction1,
+          2,
+          ethers.ZeroAddress
+        );
+    
+        await ballsToken.approve(await poolHandlerProxy.getAddress(), oneMil);
+        await poolHandlerProxy.stake(0, loosingPrediction2, 3, ethers.ZeroAddress);
+
+        //baller can stake again
+        await (poolHandlerProxy.connect(baller) as any).stake(
+          0,
+          winningPrediction,
+          1,
+          ethers.ZeroAddress
+        );
+    
+        await ballsToken.transfer(keeper, oneMil);
+        await ballsToken
+          .connect(keeper)
+          .approve(await poolHandlerProxy.getAddress(), oneMil);
+    
+        await time.increaseTo(ch.maturity);
+        const provideDataParams = prepareAssetPriceProvision(
+          ch.assetSymbol,
+          ch.maturity,
+          assetPrice
+        );
+        await registryProxy.provideData(...provideDataParams);
+    
+        //evaluate
+        await poolHandlerProxy.evaluate(0);
+
+        //dispute
+        await expect((poolDisputeProxy.connect(striker) as any).dispute(0, loosingPrediction1)).to.not.be.reverted;
+
+        //Settle
+        await expect((poolDisputeProxy.settle(0, loosingPrediction1))).to.not.be.reverted;
+        const disputedChallenge = await getChallenge(poolViewProxy, 0);
+        console.log("Disputed outcome: ", disputedChallenge);
+        expect(loosingPrediction1).to.be.equal(disputedChallenge.outcome);
   });
 });
