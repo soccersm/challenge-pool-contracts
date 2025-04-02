bc666544eb1e088b658fce60d8b9f493af9008de
baahkusi <nanakbaahkusi@gmail.com>
2025-04-02 12:00:16 +0000

    update

---
+++ ignition/modules/ArmyStakePaymaster.ts
@@ -0,0 +1,17 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const ArmyStakePaymasterModule = buildModule(
+  "ArmyStakePaymasterModule",
+  (m) => {
+    const paymaster = m.contract("StakePaymaster", [
+      m.getParameter("soccersm", "0x56d3719CcB48124d7CeE71F70B3e0bAa860E7FB6"),
+    ]);
+
+    return { paymaster };
+  }
+);
+
+export default ArmyStakePaymasterModule;
