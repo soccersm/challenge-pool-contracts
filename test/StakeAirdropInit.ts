bc9dcdf5a797a78c074e3389f0b1da511215bf92
KaePee <obedpksomuah@gmail.com>
2025-04-11 10:40:49 +0000

    init stake airdrop

--- test/StakeAirdropInit.ts
+++ test/StakeAirdropInit.ts
@@ -1,30 +1,42 @@
+import { StakeAirDropInit } from "./../typechain-types/contracts/inits/StakeAirDropInit";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, ignition } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import IgniteTestModule from "../ignition/modules/test/IgniteTest";
 
 describe("StakeAirDropInit", async function () {
   async function deployStakeAirDropInit() {
-    const [owner, user] = await ethers.getSigners();
-    const stake = await ethers.getContractFactory("StakeAirDropInit");
-    const stakePaymaster = await stake.deploy();
+    const StakeAirDropInitView = await ethers.getContractFactory(
+      "StakeAirDropInitView"
+    );
+    const stakeAirDropInitView = await StakeAirDropInitView.deploy();
+
+    const [paymaster] = await ethers.getSigners();
 
     const minStakeAmount = BigInt(1 * 1e18);
     const oneGrand = BigInt(minStakeAmount * BigInt(1e3));
 
     return {
-      owner,
-      stakePaymaster,
-      user,
+      paymaster,
+      stakeAirDropInitView,
       oneGrand,
     };
   }
 
   it("Should Deploy StakeAirDropInit", async function () {
-    const { stakePaymaster } = await loadFixture(deployStakeAirDropInit);
+    const { stakeAirDropInitView } = await loadFixture(deployStakeAirDropInit);
     expect(
-      await ethers.provider.getCode(await stakePaymaster.getAddress())
+      await ethers.provider.getCode(await stakeAirDropInitView.getAddress())
     ).to.not.equal("0x");
   });
 
-
+  it("Should check init constants", async function () {
+    const { stakeAirDropInitView, oneGrand } = await loadFixture(
+      deployStakeAirDropInit
+    );
+    console.log(
+      "getAirDropStore: ",
+      await stakeAirDropInitView.getAirDropStore()
+    );
+  });
 });
