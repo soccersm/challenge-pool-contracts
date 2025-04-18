835547ab4c27bab0c2c557c3c2a9d3d3d5e7210d
KaePee <obedpksomuah@gmail.com>
2025-04-18 23:12:12 +0000

    stake, bulk withdraw

--- test/GelatoHandlerStakeRelay.ts
+++ test/GelatoHandlerStakeRelay.ts
@@ -1,10 +1,12 @@
 import {
   ASSET_PRICES_MULTIPLIER,
   coder,
+  encodeMultiOptionByTopic,
   prepareAssetPriceProvision,
   prepareCreateChallenge,
   prepareStatementEventParam,
   prepareStatementProvision,
+  TopicId,
   yesNo,
 } from "./lib";
 import {
@@ -14,7 +16,7 @@ import {
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { toUtf8Bytes } from "ethers";
-import { btcEvent } from "./mock";
+import { btcEvent, ethPriceRange } from "./mock";
 import { deploySoccersm } from "./SoccersmDeployFixture";
 import { FacetCutAction, functionSelectors } from "../ignition/lib";
 
@@ -243,6 +245,7 @@ describe("GelatoHandler: stakeRelay, withdrawRelay", async function () {
       poolViewProxy,
       gelatoHandlerProxy,
       poolManagerProxy,
+      psProxy,
       registryProxy,
       oneMil,
     } = await loadFixture(deployGelatoHandler);
@@ -305,10 +308,149 @@ describe("GelatoHandler: stakeRelay, withdrawRelay", async function () {
     await poolHandlerProxy.evaluate(0);
     await time.increase(60 * 60);
     await poolHandlerProxy.close(0);
-    console.log("challenge 0: ", await poolViewProxy.challenges(0));
+
+    //revert invalidChallengeId
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).withdrawRelay(1)
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "InvalidChallenge");
+
+    //revert on pause
+    await psProxy.pause();
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).withdrawRelay(0)
+    ).to.be.revertedWithCustomError(psProxy, "Pausable__Paused");
+    await psProxy.unpause();
 
     //withdraw
     await expect((gelatoHandlerProxy.connect(baller) as any).withdrawRelay(0))
       .to.not.be.reverted;
+  });
+
+  it("Should bulkWithdrawRelay", async function () {
+    const {
+      ballsToken,
+      oneGrand,
+      baller,
+      poolHandlerProxy,
+      poolViewProxy,
+      gelatoHandlerProxy,
+      poolManagerProxy,
+      psProxy,
+      registryProxy,
+      oneMil,
+    } = await loadFixture(deployGelatoHandler);
+
+    //create -> stake -> close -> withdraw
+    //create BTC challenge
+    const btcChallenge = btcEvent(
+      await ballsToken.getAddress(),
+      1,
+      oneGrand,
+      ethers.ZeroAddress
+    );
+    const preparedBTCChallenge = prepareCreateChallenge(btcChallenge.challenge);
+
+    await ballsToken
+      .connect(baller)
+      .approve(
+        await poolHandlerProxy.getAddress(),
+        (
+          await poolViewProxy.createFee(oneGrand)
+        )[1]
+      );
+
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).createChallengeRelay(
+        ...(preparedBTCChallenge as any)
+      )
+    ).to.not.be.reverted;
+
+    //create ETH challenge
+    const ethPriceRangeChallenge = ethPriceRange(
+      await ballsToken.getAddress(),
+      1,
+      oneGrand,
+      ethers.ZeroAddress
+    );
+    const preparedETHChallenge = prepareCreateChallenge(
+      ethPriceRangeChallenge.challenge
+    );
+    await ballsToken
+      .connect(baller)
+      .approve(
+        await poolHandlerProxy.getAddress(),
+        (
+          await poolViewProxy.createFee(oneGrand)
+        )[1]
+      );
+    await (poolHandlerProxy.connect(baller) as any).createChallenge(
+      ...(preparedETHChallenge as any)
+    );
+
+    //stake btc
+    const prediction = yesNo.no;
+
+    const stakeParams = {
+      challengeId: 0,
+      prediction: prediction,
+      quantity: 1,
+      paymaster: ethers.ZeroAddress,
+    };
+
+    await ballsToken
+      .connect(baller)
+      .approve(await gelatoHandlerProxy.getAddress(), oneMil);
+
+    await (gelatoHandlerProxy.connect(baller) as any).stakeRelay(
+      stakeParams.challengeId,
+      stakeParams.prediction,
+      stakeParams.quantity,
+      stakeParams.paymaster
+    );
+
+    //stake eth
+    const ethPrediction = encodeMultiOptionByTopic(
+      TopicId.MultiAssetRange,
+      [6000, 7000]
+    );
+    await (gelatoHandlerProxy.connect(baller) as any).stakeRelay(
+      1,
+      ethPrediction,
+      2,
+      ethers.ZeroAddress
+    );
+
+    //btc: register, evaluate, close
+    await time.increaseTo(btcChallenge.maturity);
+    const assetPrice = 110000 * ASSET_PRICES_MULTIPLIER;
+    const provideDataParams = prepareAssetPriceProvision(
+      btcChallenge.assetSymbol,
+      btcChallenge.maturity,
+      assetPrice
+    );
+    await registryProxy.provideData(...provideDataParams);
+
+    await poolHandlerProxy.evaluate(0);
+    await time.increase(60 * 60);
+    await poolHandlerProxy.close(0);
+
+    //eth: register, evaluate, close
+    const ethAssetPrice = 6500 * ASSET_PRICES_MULTIPLIER;
+    const ethProvideDataPrams = prepareAssetPriceProvision(
+      ethPriceRangeChallenge.assetSymbol,
+      ethPriceRangeChallenge.maturity,
+      ethAssetPrice
+    );
+    await registryProxy.provideData(...ethProvideDataPrams);
+
+    await poolHandlerProxy.evaluate(1);
+    await time.increase(60*60);
+    await poolHandlerProxy.close(1);
+
+
+    //bulkwithdraw
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).bulkWithdrawRelay([0,1])
+    ).to.not.be.reverted;
   });
 });
