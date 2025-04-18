15ed6f6f2f9d332a832944d594f5a2bf98afd193
KaePee <obedpksomuah@gmail.com>
2025-04-18 10:03:34 +0000

    yes/no, multi pool init

--- test/GelatoHandler.ts
+++ test/GelatoHandler.ts
@@ -1,11 +1,20 @@
-import { prepareCreateChallenge } from "./lib";
+
+import {
+  AssetPriceBoundedEvent,
+  AssetPriceTargetEvent,
+  coder,
+  prepareCreateChallenge,
+  TopicId,
+} from "./lib";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { toUtf8Bytes } from "ethers";
-import { btcEvent } from "./mock";
+import { btcEvent, multiCorrectScore } from "./mock";
 import { deploySoccersm } from "./SoccersmDeployFixture";
 import { FacetCutAction, functionSelectors } from "../ignition/lib";
+import { GelatoHandler__factory } from "../typechain-types";
+import { math } from "../typechain-types/@openzeppelin/contracts/utils";
 
 describe("GelatoHandler", async function () {
   async function deployGelatoHandler() {
@@ -73,7 +82,6 @@ describe("GelatoHandler", async function () {
       basePrice?: BigInt;
       paymaster?: string;
       deadline?: number;
-      expectedError?: string;
     },
     fixture: Awaited<ReturnType<typeof deployGelatoHandler>>
   ) {
@@ -84,12 +92,11 @@ describe("GelatoHandler", async function () {
       poolHandlerProxy,
       poolViewProxy,
       gelatoHandlerProxy,
-      poolManagerProxy,
       baller,
       oneGrand,
     } = fixture;
 
-    // params or defaults
+    //defaults
     const stakeTokenAddress = stakeToken || (await ballsToken.getAddress());
     const quantityVal = quantity ?? 1;
     const basePriceVal = basePrice ?? oneGrand;
@@ -135,20 +142,139 @@ describe("GelatoHandler", async function () {
 
   it("Should createChallengeRelay", async function () {
     const fixture = await loadFixture(deployGelatoHandler);
+    const { poolManagerProxy } = fixture;
+
     await testCreateChallengeRelay({}, fixture);
-    expect(await fixture.poolManagerProxy.challengeId()).to.equals(1);
+    expect(await poolManagerProxy.challengeId()).to.equals(1);
   });
 
-  it("Should Reverts for createChallengeRelay", async function () {
+  it("Should Modifier Reverts for createChallengeRelay", async function () {
     const fixture = await loadFixture(deployGelatoHandler);
     //revert for whenNotPaused
-    const { psProxy } = fixture;
+    const { psProxy, gelatoHandlerProxy } = fixture;
     await psProxy.pause();
 
     await expect(
       testCreateChallengeRelay({}, fixture)
     ).to.be.revertedWithCustomError(psProxy, "Pausable__Paused");
 
-    //revert for nonZero quantity
+    await psProxy.unpause();
+
+    //revert for Zero quantity
+    await expect(
+      testCreateChallengeRelay({ quantity: 0 }, fixture)
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "ZeroNumber");
+
+    //revert for zero basePrice
+    await expect(testCreateChallengeRelay({ basePrice: 0n }, fixture)).to.be
+      .reverted;
+
+    //revert for positiveAddress and supportedToken
+    await expect(
+      testCreateChallengeRelay({ stakeToken: ethers.ZeroAddress }, fixture)
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "ZeroAddress");
+
+    await expect(
+      testCreateChallengeRelay(
+        { stakeToken: ethers.Wallet.createRandom().address },
+        fixture
+      )
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "UnsupportedToken");
+
+    //revert for validStake
+    const halfGrand = BigInt(5e17);
+    await expect(
+      testCreateChallengeRelay({ basePrice: halfGrand }, fixture)
+    ).to.be.revertedWithCustomError(
+      gelatoHandlerProxy,
+      "StakeLowerThanMinimum"
+    );
   });
+
+  it("Should revert for createChallengeRelay", async function () {
+    const {
+      ballsToken,
+      oneGrand,
+      baller,
+      poolHandlerProxy,
+      poolViewProxy,
+      gelatoHandlerProxy,
+    } = await loadFixture(deployGelatoHandler);
+    const btcChallenge = btcEvent(
+      await ballsToken.getAddress(),
+      1,
+      oneGrand,
+      ethers.ZeroAddress
+    );
+
+    const invalidEventChallenge = structuredClone(btcChallenge.challenge);
+    invalidEventChallenge.events = [];
+
+    const invalidEventsPreparedBTCChallenge = prepareCreateChallenge(
+      invalidEventChallenge
+    );
+
+    await ballsToken
+      .connect(baller)
+      .approve(
+        await poolHandlerProxy.getAddress(),
+        (
+          await poolViewProxy.createFee(oneGrand)
+        )[1]
+      );
+
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).createChallengeRelay(
+        ...invalidEventsPreparedBTCChallenge
+      )
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "InvalidEventLength");
+
+    //revert InvalidEventMaturity
+    const invalidMaturityEvent = structuredClone(btcChallenge.challenge);
+    invalidMaturityEvent.events[0].maturity =
+      Math.floor(Date.now() / 1000) + 60;
+
+    const invalidMaturityPreparedBTCChallenge =
+      prepareCreateChallenge(invalidMaturityEvent);
+
+    await expect(
+      (gelatoHandlerProxy.connect(baller) as any).createChallengeRelay(
+        ...invalidMaturityPreparedBTCChallenge
+      )
+    ).to.be.revertedWithCustomError(gelatoHandlerProxy, "InvalidEventMaturity");
+
+  });
+
+  it("Should revert for createChallengeRelay: multi", async function() {
+    const {
+      ballsToken,
+      oneGrand,
+      baller,
+      poolHandlerProxy,
+      poolViewProxy,
+      gelatoHandlerProxy,
+    } = await loadFixture(deployGelatoHandler);
+
+    //multi event
+     const multiCorrectScoreChallenge = multiCorrectScore(
+          await ballsToken.getAddress(),
+          1,
+          oneGrand,
+          ethers.ZeroAddress
+        );
+        const preparedMultiCorrectScoreChallenge = prepareCreateChallenge(
+          multiCorrectScoreChallenge.challenge
+        );
+        await ballsToken
+          .connect(baller)
+          .approve(
+            await poolHandlerProxy.getAddress(),
+            (
+              await poolViewProxy.createFee(oneGrand)
+            )[1]
+          );
+        
+        await expect((gelatoHandlerProxy.connect(baller)as any).createChallengeRelay(...preparedMultiCorrectScoreChallenge)).to.not.be.reverted;
+      
+  })
 });
