9157c1efebcc573fb7c491b34791db6c38f275f6
KaePee <obedpksomuah@gmail.com>
2025-05-27 01:06:11 +0000

    updates

---
+++ contracts/utils/CommunityHelpers.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "../interfaces/ICommunity.sol";
+import "../libraries/LibData.sol";
+
+abstract contract CommunityHelpers {
+    modifier notBanned(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        ICommunity.Community storage community = s.communities[_communityId];
+        if (community.banned) {
+            revert ICommunity.CommunityIsBanned();
+        }
+        _;
+    }
+
+    modifier onlyCommunityOwner(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        ICommunity.Community storage community = s.communities[_communityId];
+        if (msg.sender != s.communities[_communityId].owner) {
+            revert ICommunity.NotCommunityOwner();
+        }
+        _;
+    }
+
+    modifier communityOwnerOrAdmin(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        ICommunity.Community storage community = s.communities[_communityId];
+
+        if (msg.sender == community.owner) {
+            _;
+            return;
+        }
+
+        if (!s.isAdmin[_communityId][msg.sender]) {
+            revert ICommunity.NotCommunityOwnerOrAdmin(
+                _communityId,
+                msg.sender
+            );
+        }
+        _;
+    }
+    modifier onlyCommunityAdmin(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        ICommunity.Community storage community = s.communities[_communityId];
+        if (!s.isAdmin[_communityId][msg.sender]) {
+            revert ICommunity.NotCommunityAdmin();
+        }
+        _;
+    }
+
+    modifier onlyCommunityMember(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        if (!s.isMember[_communityId][msg.sender]) {
+            revert ICommunity.NotCommunityMember();
+        }
+        _;
+    }
+
+    modifier isCommunityMember(string calldata _communityId, address _member) {
+        CommunityStore storage s = CommunityStorage.load();
+        if (!s.isMember[_communityId][_member]) {
+            revert ICommunity.NotCommunityMember();
+        }
+        _;
+    }
+
+    modifier communityExists(string calldata _communityId) {
+        CommunityStore storage s = CommunityStorage.load();
+        if (s.communities[_communityId].owner == address(0)) {
+            revert ICommunity.CommunityDoesNotExist(_communityId);
+        }
+        _;
+    }
+}
