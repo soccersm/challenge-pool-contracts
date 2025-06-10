4d9c8fb4cb637840e8564b13b72d258b2919e805
KaePee <obedpksomuah@gmail.com>
2025-06-10 14:51:13 +0000

    function updates

--- contracts/interfaces/ICommunity.sol
+++ contracts/interfaces/ICommunity.sol
@@ -4,7 +4,7 @@ import "../interfaces/IChallengePoolHandler.sol";
 
 abstract contract ICommunity {
     struct Community {
-        bytes communityId;
+        string communityId;
         address owner;
         address pendingOwner;
         uint256 memberCount;
@@ -12,35 +12,44 @@ abstract contract ICommunity {
         bytes32 tournamentId;
     }
 
-    event NewCommunity(bytes communityId, address owner, uint256 memberCount, bool bannedStatus, uint256 createdAt);
+    event NewCommunity(
+        string _communityId,
+        bytes32 communityId,
+        address owner,
+        uint256 memberCount,
+        bool bannedStatus,
+        uint256 createdAt
+    );
 
-    event CommunityBanned(bytes communityId, address council);
-    event CommunityUnBanned(bytes communityId, address council);
-    event AdminAdded(bytes communityId, address caller, address newAdmin);
-    event AdminRemoved(bytes communityId, address caller, address admin);
-    event MemberJoined(bytes communityId, address member, uint256 timeAt);
-    event MemberRemoved(bytes communityId, address member, uint256 timeAt);
+    event CommunityBanned(bytes32 communityId, address council);
+    event CommunityUnBanned(bytes32 communityId, address council);
+    event AdminAdded(bytes32 communityId, address caller, address newAdmin);
+    event AdminRemoved(bytes32 communityId, address caller, address admin);
+
     event CommunityOwnerTransferAccepted(
-        bytes communityId,
+        bytes32 communityId,
         address previousOwner,
         address newOwner,
         uint256 timeAt
     );
     event CommunityOwnerTransferInitiated(
-        bytes communityId,
+        bytes32 communityId,
         address oldOwner,
         address newOwner,
         uint256 timeAt
     );
     event EvaluateCommunityChallenge(
         uint256 challengeId,
+        bytes32 communityId,
         address evaluator,
         IChallengePoolHandler.ChallengeState state,
         bytes result
     );
-    event MemberIsBanned(bytes communityId, address user, uint256 timeAt);
-    event MemberUnbanned(bytes communityId, address user, uint256 timeAt);
-    event MemberLeftCommunity(bytes communityId, address user, uint256 timeAt);
+    event MemberIsBanned(bytes32 communityId, address user, uint256 timeAt);
+    event MemberUnbanned(bytes32 communityId, address user, uint256 timeAt);
+    event MemberJoined(bytes32 communityId, address member, uint256 timeAt);
+    event MemberLeftCommunity(bytes32 communityId, address user, uint256 timeAt);
+    event MemberRemoved(bytes32 communityId, address member, uint256 timeAt);
 
     error CommunityIsBanned();
     error CommunityNotBanned();
@@ -48,9 +57,9 @@ abstract contract ICommunity {
     error NotCommunityMember();
     error AlreadyCommunityMember();
     error NotCommunityOwner();
-    error NotCommunityOwnerOrAdmin(bytes communityId, address caller);
-    error CommunityDoesNotExist(bytes communityId);
-    error AlreadyCommunityAdmin(bytes communityId, address admin);
+    error NotCommunityOwnerOrAdmin(bytes32 communityId, address caller);
+    error CommunityDoesNotExist(bytes32 communityId);
+    error AlreadyCommunityAdmin();
     error MustBeCommunityAdmin();
     error CommunityChallengeRequiresCommunity();
     error CommunityAlreadyExists();
@@ -60,15 +69,15 @@ abstract contract ICommunity {
      * @dev Creates a new community with the specified name.
      * @param _communityId The id of the community to be created.
      */
-    function createCommunity(bytes calldata _communityId) external virtual;
+    function createCommunity(string calldata _communityId) external virtual;
 
     /**
      * @dev adds an admin to the community
-     * @param _communityId community to add an admin
+     * @param _communityId of the community to add an admin
      * @param _admin the user to make an admin
      */
     function addCommunityAdmin(
-        bytes calldata _communityId,
+        bytes32 _communityId,
         address _admin
     ) external virtual;
 
@@ -78,7 +87,7 @@ abstract contract ICommunity {
      * @param _admin the address of the admin to remove
      */
     function removeCommunityAdmin(
-        bytes calldata _communityId,
+        bytes32 _communityId,
         address _admin
     ) external virtual;
 
@@ -86,55 +95,49 @@ abstract contract ICommunity {
      * @dev ban a community
      * @param _communityId community to ban
      */
-    function banCommunity(bytes calldata _communityId) external virtual;
+    function banCommunity(bytes32 _communityId) external virtual;
 
     /**
      * @dev unban a banned community
      * @param _communityId of the banned community to unban
      */
-    function unBanCommunity(bytes calldata _communityId) external virtual;
+    function unBanCommunity(bytes32 _communityId) external virtual;
 
     /**
      * @dev ban a user and prevent user from joining community
      * @param _communityId community user belongs to
      * @param _user address of user to ban
      */
-    function banMember(
-        bytes calldata _communityId,
-        address _user
-    ) external virtual;
+    function banMember(bytes32 _communityId, address _user) external virtual;
 
     /**
      * @dev unban a banned user
      * @param _communityId community user belongs to
      * @param _user address of user to unban
      */
-    function unBanMember(
-        bytes calldata _communityId,
-        address _user
-    ) external virtual;
+    function unBanMember(bytes32 _communityId, address _user) external virtual;
 
     /**
      * @dev user joins a community
      * @param _communityId community to join
      */
-    function joinCommunity(bytes calldata _communityId) external virtual;
+    function joinCommunity(bytes32 _communityId) external virtual;
 
     /**
      * @dev enables a user to leave a community
      * @param _communityId id of the community the user wants to leave
      */
 
-    function leaveCommunity(bytes calldata _communityId) external virtual;
+    function leaveCommunity(bytes32 _communityId) external virtual;
 
     /**
      * @dev removes a user from a community;
      * @param _communityId communityId to remove the user from
-     * @param _user the address of the user to remove from community
+     * @param _member the address of the member to remove from community
      */
     function removeCommunityMember(
-        bytes calldata _communityId,
-        address _user
+        bytes32 _communityId,
+        address _member
     ) external virtual;
 
     /**
@@ -145,7 +148,7 @@ abstract contract ICommunity {
      */
 
     function transferCommunityOwner(
-        bytes calldata _communityId,
+        bytes32 _communityId,
         address _owner
     ) external virtual;
 
@@ -153,11 +156,9 @@ abstract contract ICommunity {
      * @notice accept pending community ownership transfer
      * @dev only callable by the stored pendingOwner
      * @param _communityId the id of the community to accept ownership transfer
-     * 
+     *
      */
-    function acceptCommunityOwnership(
-        bytes calldata _communityId
-    ) external virtual;
+    function acceptCommunityOwnership(bytes32 _communityId) external virtual;
 
     function evaluateCustomChallenge(
         uint256 _challengeId,
--- contracts/interfaces/ICommunityView.sol
+++ contracts/interfaces/ICommunityView.sol
@@ -5,32 +5,32 @@ import "../interfaces/ICommunity.sol";
 
 interface ICommunityView {
     function getCommunity(
-        bytes calldata _communityId
+        bytes32 _communityId
     ) external view returns (ICommunity.Community memory);
 
     function getIsAdmin(
-        bytes calldata _communityId,
+        bytes32 _communityId,
         address _admin
     ) external view returns (bool);
 
     function getIsMember(
-        bytes calldata _communityId,
+        bytes32 _communityId,
         address _member
     ) external view returns (bool);
 
     function getBanStatus(
-        bytes calldata _communityId
+        bytes32 _communityId
     ) external view returns (bool);
 
     function getMembersCount(
-        bytes calldata _communityId
+        bytes32 _communityId
     ) external view returns (uint256);
 
     function getOwnerAddress(
-        bytes calldata _communityId
+        bytes32 _communityId
     ) external view returns (address);
 
     function getPendingOwnerAddress(
-        bytes calldata _communityId
+        bytes32 _communityId
     ) external view returns (address);
 }
