7d1000230f42e681a0c1411d11d469815d6ccc8e
baahkusi <nanakbaahkusi@gmail.com>
2025-06-30 15:25:06 +0000

    update

---
+++ ignition/modules/updates/ChallengePoolHandler_CommunityChallengeEventUpdateModule.ts
@@ -0,0 +1,40 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import {
+  functionSigsSelectors,
+  functionSelectors,
+  FacetCutAction,
+  INIT_SIG,
+} from "../../lib";
+import Soccersm from "../Soccersm";
+import { ethers } from "hardhat";
+
+const ChallengePoolHandler_CommunityChallengeEventUpdateModule = buildModule(
+  "ChallengePoolHandler_CommunityChallengeEventUpdateModule",
+  (m) => {
+    const soccersm = "0x56d3719CcB48124d7CeE71F70B3e0bAa860E7FB6";
+    const cutProxy = m.contractAt("DiamondCutFacet", soccersm);
+
+    const cphS = functionSelectors("ChallengePoolHandler");
+    const cph = m.contract("ChallengePoolHandler");
+    const cphC = [cph, FacetCutAction.Replace, cphS];
+
+    m.call(
+      cutProxy,
+      "diamondCut",
+      [[cphC], ethers.ZeroAddress, ethers.ZeroHash],
+      { id: "ChallengePoolDiamondCut" }
+    );
+
+    const poolHandlerProxy = m.contractAt("ChallengePoolHandler", soccersm, {
+      id: "SoccersmChallengePoolHandler",
+    });
+
+    return {
+      poolHandlerProxy,
+    };
+  }
+);
+
+export default ChallengePoolHandler_CommunityChallengeEventUpdateModule;
