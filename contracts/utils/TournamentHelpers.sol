fdea4ec4e7ad337033866c9e299b61ff2ad346b4
kaepee <obedpksomuah@gmail.com>
2025-07-22 22:16:06 +0000

    helpers

---
+++ contracts/utils/TournamentHelpers.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+import "contracts/interfaces/ITournament.sol";
+import "contracts/libraries/LibData.sol";
+
+abstract contract TournamentHelpers {
+    modifier tournamentExists(bytes32 _id) {
+        TournamentStore storage ts = TournamentStorage.load();
+        ITournament.Tournament storage t = ts.tournaments[_id];
+        if (t.id == bytes32(0)) {
+            revert ITournament.TournamentDoesNotExist();
+        }
+        _;
+    }
+
+    modifier validPeriod(uint256 _startTime, uint256 _endTime) {
+        if (_startTime < block.timestamp || _endTime < block.timestamp) {
+            revert ITournament.InvalidPeriod();
+        }
+        if (_endTime <= _startTime) {
+            revert ITournament.InvalidPeriod();
+        }
+        _;
+    }
+
+    modifier pendingTournament(bytes32 _id) {
+        TournamentStore storage ts = TournamentStorage.load();
+        ITournament.Tournament storage t = ts.tournaments[_id];
+        if(t.startTime > block.timestamp) {
+            revert ITournament.TournamentAlreadyStarted();
+        }
+        _;
+    }
+
+    modifier tournamentAdmin(bytes32 _id, address _admin) {
+        TournamentStore storage ts = TournamentStorage.load();
+        if (!ts.isAdmin[_id][_admin]) {
+            revert ITournament.MustBeTournamentAdmin();
+        }
+        _;
+    }
+
+    modifier onlyTournamentOwner(bytes32 _id) {
+        TournamentStore storage ts = TournamentStorage.load();
+        ITournament.Tournament storage t = ts.tournaments[_id];
+        if (t.creator != msg.sender) {
+            revert ITournament.NotTournamentOwner();
+        }
+        _;
+    }
+
+    modifier tournamentOwnerOrAdmin(bytes32 _id) {
+        TournamentStore storage ts = TournamentStorage.load();
+        ITournament.Tournament storage t = ts.tournaments[_id];
+        bool admin = ts.isAdmin[_id][msg.sender];
+        bool owner = t.creator == msg.sender;
+        if (!owner && !admin) {
+            revert ITournament.NotTournamentOwnerOrAdmin();
+        }
+        _;
+    }
+
+    modifier tournamentNotBanned(bytes32 _id) {
+        TournamentStore storage ts = TournamentStorage.load();
+        ITournament.Tournament storage t = ts.tournaments[_id];
+        if (t.banned) {
+            revert ITournament.TournamentIsBanned();
+        }
+        _;
+    }
+}
